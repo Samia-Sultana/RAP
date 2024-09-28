16c19c6e1de61c65477d9c8cc9da113e8b4158cf
Samia-Sultana <samia.cse.ewu@gmail.com>
2024-09-28 14:04:46 +0600

    feat: reset password

--- routes/web.php
+++ routes/web.php
@@ -30,6 +30,8 @@ Route::post('/email/verification-notification', function (Request $request) {
     return back()->with('message', 'Verification link sent!');
 })->middleware(['auth', 'throttle:6,1'])->name('verification.send');
 
+
+
 Route::group(['middleware' => 'useradmin'], function () {
     Route::get('role', [RoleController::class, 'list']);
     Route::get('role/add', [RoleController::class, 'add']);
