49c2baf0362cca461b47ce8094e298ecad164900
Samia-Sultana <samia.cse.ewu@gmail.com>
2024-09-29 17:48:58 +0600

    feat: basic queue job implemented

---
+++ app/Jobs/SendEmail.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Jobs;
+
+use App\Mail\userAccountCreated;
+use App\Models\User;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Illuminate\Foundation\Queue\Queueable;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Queue\SerializesModels;
+use Mail;
+
+class SendEmail implements ShouldQueue
+{
+    use Queueable;
+
+    protected $user;
+
+    /**
+     * Create a new job instance.
+     *
+     * @param \App\Models\User $user
+     * @return void
+     */
+    public function __construct(User $user)
+    {
+        $this->user = $user;
+    }
+
+    /**
+     * Execute the job.
+     *
+     * @return void
+     */
+    public function handle()
+    {
+        Mail::to($this->user->email)->send(new userAccountCreated($this->user));
+    }
+}
